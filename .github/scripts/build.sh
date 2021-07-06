f23d60d859701acaf16de680ba77b55458790a77
Kiran Kumar Kotari <kirankotari@live.com>
2021-07-06 15:01:06 +0200

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,13 +3,15 @@ mkdir -p $HOME
 export PYENV="$HOME/pyenv-win"
 export PATH="$PYENV/bin:$PYENV/shims:$PATH"
 
-git clone https://github.com/pyenv-win/pyenv-win.git $HOME
-$PYENV/bin/pyenv --version
-$PYENV/bin/pyenv update
-$PYENV/bin/pyenv install -q 3.7.4
-$PYENV/bin/pyenv global 3.7.4
-$PYENV/bin/pyenv versions
-$PYENV/shims/python --version
-$PYENV/shims/pip install -r requirements_dev.txt
-$PYENV/bin/pyenv.bat rehash
-$PYENV/shims/python -m pytest -v -s --cache-clear --cov=app test
+echo $PATH
+
+# git clone https://github.com/pyenv-win/pyenv-win.git $HOME
+# $PYENV/bin/pyenv --version
+# $PYENV/bin/pyenv update
+# $PYENV/bin/pyenv install -q 3.7.4
+# $PYENV/bin/pyenv global 3.7.4
+# $PYENV/bin/pyenv versions
+# $PYENV/shims/python --version
+# $PYENV/shims/pip install -r requirements_dev.txt
+# $PYENV/bin/pyenv.bat rehash
+# $PYENV/shims/python -m pytest -v -s --cache-clear --cov=app test
